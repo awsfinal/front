167e50206446f8a4024306c5a5443f224ad3e25a
chanhk1 <changal1234@naver.com>
2025-08-01 17:23:58 +0900

    feature/#1 oauth 성공

--- src/App.js
+++ src/App.js
@@ -11,7 +11,10 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router future={{ 
+      v7_relativeSplatPath: true,
+      v7_startTransition: true 
+    }}>
       <div className="App">
         <Routes>
           <Route path="/" element={<LoginPage />} />
